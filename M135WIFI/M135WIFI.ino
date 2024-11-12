7c1907446d1a8133b134bed1ceeacdfc96842f56
Levy Souto <109307063+levyssouto@users.noreply.github.com>
2024-11-12 19:21:14 -0300

    Atualização do código

--- M135WIFI/M135WIFI.ino
+++ M135WIFI/M135WIFI.ino
@@ -3,24 +3,23 @@
 #include "MQ135.h"
 
 // Configurações do WiFi e ThingSpeak
-const char* ssid = "SOUSA_2.4GHz";         
-const char* password = "53899835";         
-WiFiClient client;
+const char* nomeRedeWiFi = "SOUSA_2.4GHz";         
+const char* senhaWiFi = "53899835";         
+WiFiClient clienteWiFi;
 
-unsigned long channelID = 2729447;         
-const char* writeAPIKey = "N18GSRXFKCBKNO3U"; 
+unsigned long idCanal = 2729447;         
+const char* chaveEscritaAPI = "N18GSRXFKCBKNO3U"; 
 
+const int pinoSensor = 34;  
+MQ135 sensorGas(pinoSensor);
 
-const int sensorPin = 34;  
-MQ135 gasSensor(sensorPin);
-
-void setup() {
+void configurar() {
     Serial.begin(9600);
     delay(10);
 
     // Conectar ao WiFi
     Serial.println("Conectando ao WiFi...");
-    WiFi.begin(ssid, password);
+    WiFi.begin(nomeRedeWiFi, senhaWiFi);
 
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
@@ -29,30 +28,30 @@ void setup() {
     Serial.println("\nWiFi conectado!");
 
     // Inicializar ThingSpeak
-    ThingSpeak.begin(client);
+    ThingSpeak.begin(clienteWiFi);
 }
 
 void loop() {
     // Leitura da qualidade do ar pelo sensor MQ135
-    int air_quality = gasSensor.getPPM();
+    int qualidadeAr = sensorGas.getPPM();
     Serial.print("Qualidade do Ar (PPM): ");
-    Serial.println(air_quality);
+    Serial.println(qualidadeAr);
 
     // Enviar dados para o ThingSpeak
     if (WiFi.status() == WL_CONNECTED) {
-        ThingSpeak.setField(1, air_quality);  // Envia o valor  para o campo 1
+        ThingSpeak.setField(1, qualidadeAr);  // Envia o valor para o campo 1
 
-        int responseCode = ThingSpeak.writeFields(channelID, writeAPIKey);
-        if (responseCode == 200) {
+        int codigoResposta = ThingSpeak.writeFields(idCanal, chaveEscritaAPI);
+        if (codigoResposta == 200) {
             Serial.println("Dados enviados com sucesso para o ThingSpeak.");
         } else {
-            Serial.println("Erro ao enviar dados: Código de resposta " + String(responseCode));
+            Serial.println("Erro ao enviar dados: Código de resposta " + String(codigoResposta));
         }
     } else {
         Serial.println("Falha na conexão WiFi. Tentando reconectar...");
-        WiFi.begin(ssid, password);
+        WiFi.begin(nomeRedeWiFi, senhaWiFi);
     }
 
-    // Intervalo  de 15 segundos 
+    // Intervalo de 15 segundos
     delay(15000);
 }
